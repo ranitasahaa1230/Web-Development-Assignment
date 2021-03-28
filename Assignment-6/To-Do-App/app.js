c0347b731331d403a302829b76ff27ac6fcdb6f9
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2021-03-29 00:09:58 +0530

    Add files via upload

---
+++ Assignment-6/To-Do-App/app.js
@@ -0,0 +1,40 @@
+let inputText = document.querySelector('input');
+const btn = document.getElementById('btn');
+const ul = document.querySelector('ul');
+
+btn.onclick = function (e){
+    console.log("Clicked");   
+    let b = inputText.value;
+    console.log(b.length);
+    
+
+
+const todoItem = inputText.value;
+
+    let li = document.createElement('li');
+    li.innerText = todoItem;
+    ul.appendChild(li);
+    inputText.value = "";
+    
+    var dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteListItem);
+    
+    function deleteListItem(){
+      
+            li.remove();
+        }
+        var edit = document.createElement("button");
+        edit.appendChild(document.createTextNode("Edit"));
+        li.appendChild(edit);
+        edit.addEventListener("click", editLi);
+        
+        function editLi(){
+             let p = prompt("Edit Your Entry");
+             li.innerText = p;
+             li.appendChild(dBtn);
+             li.appendChild(edit);
+
+        }
+    }
